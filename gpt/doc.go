fcf0100f43e7d1add65e9164af0f2457f872901a
Viktor Mörsell <viktor@upper.st>
2023-03-16 04:41:02 +0100

    Add doc in inner package

---
+++ gpt/doc.go
@@ -0,0 +1,26 @@
+/*
+Package gpt provides a client for using the OpenAI Chat API.
+
+# Usage
+
+	import "github.com/vmorsell/gpt-sdk-go/gpt"
+
+Initialize a new client.
+
+	config := gpt.NewConfig.WithAPIKey("xyz")
+	client := gpt.NewClient(config)
+
+You are now ready to make calls to the API.
+
+	res, err := client.ChatCompletion(gpt.ChatCompletionInput{
+		Messages: []gpt.Message{
+			{
+				Role:    "user",
+				Content: "Hello, World!",
+			},
+		},
+	}
+
+)
+*/
+package gpt
